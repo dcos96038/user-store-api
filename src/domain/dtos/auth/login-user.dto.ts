1c3df2fd6c9b93d7cb13af55c4efacbe376bd560
Diego Coscolla <diegocoscolla@gmail.com>
2023-11-27 21:23:55 -0300

    Fix password length validation in LoginUserDto

--- src/domain/dtos/auth/login-user.dto.ts
+++ src/domain/dtos/auth/login-user.dto.ts
@@ -17,7 +17,6 @@ export class LoginUserDto {
     if (!email) return ['Email is required', undefined] as const
     if (!regularExps.email.test(email)) return ['Email is invalid', undefined] as const
     if (!password) return ['Password is required', undefined] as const
-    if (password.length < 8) return ['Password must be at least 8 characters', undefined] as const
 
     return [undefined, new LoginUserDto(email, password)] as const
   }
